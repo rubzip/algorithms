72ee733844a4214f0c85658c51b20e4c9669dd96
Rubén <74114026+rubzip@users.noreply.github.com>
2022-07-27 21:10:48 +0200

    Add files via upload

--- 1.Divide_and_Conquer_Sorting_and_Searching_and_Randomized_Algorithms/merge_sort.py
+++ 1.Divide_and_Conquer_Sorting_and_Searching_and_Randomized_Algorithms/merge_sort.py
@@ -1,39 +1,40 @@
-def split(x: list):
-    n = int(len(x) / 2)
+
+def split(x:list)->tuple:
+    n = int(len(x)/2)
+    
     return x[:n], x[n:]
 
-
-def merge(x: list, y: list) -> list:
-    z = list()
-    lx, ly = len(x), len(y)
-    i, j = 0, 0
-    while i < lx or j < ly:
-        if i == lx:
-            # If x is 100% inserted, I insert all remaining values from y
-            z += y[j:]
-            j = ly
-        elif j == ly:
-            # If y is 100% inserted, I insert all remaining values from x
-            z += x[i:]
-            i = lx
-        elif x[i] < y[j]:
-            z.append(x[i])
-            i += 1
-        else:
-            z.append(y[j])
-            j += 1
-    return z
-
-
-def merge_sort(x: list) -> list:
+def sort(x:list)->list:
     if len(x) == 1:
         return x
-    elif len(x) == 2:
-        if x[0] > x[1]:
+        if x[0]>x[1]:
             return [x[1], x[0]]
         else:
             return x
     else:
-        y, z = split(x)
-        y_s, z_s = merge_sort(y), merge_sort(z)
-        return merge(y_s, z_s)
+        pass
+
+def merge_sort(x:list):
+    y, z = split(x)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    if len(x)>1 and len(y)>1:
+        pass
+    else:
+        if len(x)==1:
+            pass
+        elif len(y)
